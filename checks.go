ca38598d5d00c43ba35ea6ca0c1528e81dffd929
Farzad Ghanei <644113+farzadghanei@users.noreply.github.com>
2022-03-20 14:58:13 +0100

    add checks types, file and basic dial checks

---
+++ checks.go
@@ -0,0 +1,231 @@
+package chkok
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"syscall"
+	"time"
+)
+
+type Status uint8
+
+const (
+	StatusUnknown Status = iota
+	StatusRunning
+	StatusStopped
+	StatusDone
+)
+
+type FileType uint8
+
+const (
+	TypeAny FileType = iota
+	TypeFile
+	TypeDir
+)
+
+type Result struct {
+	IsOK   bool
+	Issues []error
+}
+
+type Check interface {
+	Group() string
+	Name() string
+	Run() Result
+	Result() Result
+	Status() Status
+}
+
+type Checks []Check
+
+type baseCheck struct {
+	group  string
+	name   string
+	status Status
+	result Result
+}
+
+func (bc *baseCheck) Group() string {
+	return bc.group
+}
+
+func (bc *baseCheck) Name() string {
+	return bc.name
+}
+
+func (bc *baseCheck) Result() Result {
+	return bc.result
+}
+
+func (bc *baseCheck) Status() Status {
+	return bc.status
+}
+
+// CheckFile checks for file/dir existence/type/uid/gid/size
+type CheckFile struct {
+	baseCheck
+	path     string
+	fileType FileType
+	uid      int32 // -1 to skip
+	gid      int32 // -1 to skip
+	absent   bool
+	minSize  int32 // -1 to skip
+	maxSize  int64 // -1 to skip
+}
+
+// NewCheckFile returns a new checkFile without a uid/gid/size checks
+func NewCheckFile(path string) *CheckFile {
+	return &CheckFile{path: path, fileType: TypeAny, uid: -1, gid: -1, absent: false, minSize: -1, maxSize: -1}
+}
+
+func (chk *CheckFile) typeString() string {
+	switch chk.fileType {
+	case TypeFile:
+		return "file"
+	case TypeDir:
+		return "dir"
+	}
+	return "any"
+}
+
+func (chk *CheckFile) Name() string {
+	return fmt.Sprintf("%v:%v", chk.typeString(), chk.path)
+}
+
+// checkFile.Run runs the check
+func (chk *CheckFile) Run() Result {
+	if chk.path == "" {
+		panic("check file path is empty")
+	}
+
+	chk.status = StatusRunning
+	result := Result{IsOK: true, Issues: []error{}}
+	finfo, err := os.Lstat(chk.path)
+	if chk.absent { // file is not there
+		chk.status = StatusDone
+		return result
+	}
+	if err != nil {
+		result.IsOK = false
+		result.Issues = append(result.Issues, err)
+		chk.status = StatusDone
+		return result
+	}
+
+	var fstat *syscall.Stat_t = finfo.Sys().(*syscall.Stat_t)
+
+	switch chk.fileType {
+	case TypeDir:
+		if !finfo.IsDir() {
+			result.IsOK = false
+			result.Issues = append(result.Issues, errors.New("is not a directory"))
+		}
+	case TypeFile:
+		if !finfo.Mode().IsRegular() {
+			result.IsOK = false
+			result.Issues = append(result.Issues, errors.New("is not a regular file"))
+		}
+	}
+
+	chk.checkUidGid(fstat, &result)
+	chk.checkSize(finfo.Size(), &result)
+	chk.status = StatusDone
+	return result
+}
+
+// checkFile.checkUidGid checks for file uid/gid attrs updates the provided result
+func (chk *CheckFile) checkUidGid(fstat *syscall.Stat_t, result *Result) {
+	if chk.uid > -1 {
+		if fstat == nil {
+			result.IsOK = false
+			result.Issues = append(result.Issues, fmt.Errorf("check for file owner is not supported on this system"))
+		} else if uint32(chk.uid) != fstat.Uid {
+			result.IsOK = false
+			result.Issues = append(result.Issues, fmt.Errorf("owner mismatch. want %v got %v", chk.uid, fstat.Uid))
+		}
+	}
+
+	if chk.gid > -1 {
+		if fstat == nil {
+			result.IsOK = false
+			result.Issues = append(result.Issues, fmt.Errorf("check for file group is not supported on this system"))
+		} else if uint32(chk.gid) != fstat.Gid {
+			result.IsOK = false
+			result.Issues = append(result.Issues, fmt.Errorf("group mismatch. want %v got %v", chk.gid, fstat.Gid))
+		}
+	}
+}
+
+// checkFile.checkSize checks for file min/max size and updates the provided result
+func (chk *CheckFile) checkSize(size int64, result *Result) {
+	if chk.minSize > -1 && size <= int64(chk.minSize) {
+		result.IsOK = false
+		result.Issues = append(result.Issues, fmt.Errorf("file too small, size %v is less than min size %v", size, chk.minSize))
+	}
+	if chk.maxSize > -1 && size >= chk.maxSize {
+		result.IsOK = false
+		result.Issues = append(result.Issues, fmt.Errorf("file too large, size %v is more than max size %v", size, chk.maxSize))
+	}
+}
+
+// CheckDial checks for a net resource by dialing
+type CheckDial struct {
+	baseCheck
+	Network string
+	Address string
+	Absent  bool
+	Timeout time.Duration
+}
+
+// NewCheckDial returns a checkDial for local http availablity by default
+func NewCheckDial() *CheckDial {
+	timeout, err := time.ParseDuration("5s")
+	if err != nil {
+		panic("err creating check dial default timeout")
+	}
+	check := CheckDial{Network: "tcp", Address: "127.0.0.1:80", Absent: false}
+	check.Timeout = timeout
+	return &check
+}
+
+func (chk *CheckDial) Name() string {
+	return fmt.Sprintf("%v:%v", chk.Network, chk.Address)
+}
+
+// checkDial.Run runs the check and returns the results
+func (chk *CheckDial) Run() Result {
+	if chk.Network == "" {
+		panic("check dial network is empty")
+	}
+	if chk.Address == "" {
+		panic("check dial address is empty")
+	}
+
+	start := time.Now()
+	result := Result{IsOK: true, Issues: []error{}}
+	conn, err := net.DialTimeout(chk.Network, chk.Address, chk.Timeout)
+	if err != nil { // no connection
+		if chk.Absent {
+			chk.status = StatusDone
+			return result
+		} else {
+			result.IsOK = false
+			result.Issues = append(result.Issues, err)
+			chk.status = StatusDone
+			return result
+		}
+	}
+	defer conn.Close()
+	elapsed := time.Since(start)
+	if elapsed > chk.Timeout {
+		chk.status = StatusStopped
+		result.IsOK = false
+		result.Issues = append(result.Issues, fmt.Errorf("check dial timed out after %v seconds", elapsed.Seconds()))
+	} else {
+		chk.status = StatusDone
+	}
+	return result
+}
---
+++ checks_test.go
@@ -0,0 +1,203 @@
+package chkok
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+)
+
+const UNAVAILABLE_UID = 10375 // hopefully this won't be the uid on the running host
+const UNAVAILABLE_GID = 10375 // hopefully this won't be the gid on the running host
+const UNAVAILABLE_PORT = 1023 // hopefully this port is not open on the running host
+
+func TestCheckFile(t *testing.T) {
+	var check *CheckFile
+	var got, want string
+	var gotStatus, wantStatus Status
+	wantStatus = StatusUnknown
+	check = NewCheckFile("/no/such/path/exists")
+	if gotStatus = check.Status(); gotStatus != wantStatus {
+		t.Errorf("invalid check dial status, want %v got %v", wantStatus, gotStatus)
+	}
+	if got = check.Group(); got != want {
+		t.Errorf("invalid check file group, want empty got %v", got)
+	}
+	want = "any:/no/such/path/exists"
+	if got = check.Name(); got != want {
+		t.Errorf("invalid check file name, want %v got %v", want, got)
+	}
+	check.fileType = TypeDir
+	want = "dir:/no/such/path/exists"
+	if got = check.Name(); got != want {
+		t.Errorf("invalid check dir name, want %v got %v", want, got)
+	}
+}
+
+func TestCheckFileFiles(t *testing.T) {
+	var check *CheckFile
+	var got Result
+	var gotStatus, wantStatus Status
+	wantStatus = StatusDone
+	check = NewCheckFile("/no/such/path/exists")
+	if check.Run().IsOK {
+		t.Error("invalid check file not exists, want not ok got ok")
+	}
+	if gotStatus = check.Status(); gotStatus != wantStatus {
+		t.Errorf("invalid check file status, want %v got %v", wantStatus, gotStatus)
+	}
+
+	check = NewCheckFile("/no/such/path/exists")
+	check.absent = true
+	if !check.Run().IsOK {
+		t.Error("invalid check file not exists, want ok got not ok")
+	}
+	if gotStatus = check.Status(); gotStatus != wantStatus {
+		t.Errorf("invalid check file status, want %v got %v", wantStatus, gotStatus)
+	}
+
+	check = NewCheckFile("LICENSE")
+	if got = check.Run(); !got.IsOK {
+		t.Error("invalid check file, want ok got not ok")
+	} else if len(got.Issues) > 0 {
+		t.Errorf("invalid check file, want no issues got %v", got.Issues)
+	}
+	if gotStatus = check.Status(); gotStatus != wantStatus {
+		t.Errorf("invalid check file status, want %v got %v", wantStatus, gotStatus)
+	}
+
+	check.uid = UNAVAILABLE_UID
+	if got = check.Run(); got.IsOK {
+		t.Error("invalid check file uid, want not ok got ok")
+	} else if len(got.Issues) != 1 || !strings.Contains(fmt.Sprintf("%v", got.Issues[0]), "owner mismatch") {
+		t.Errorf("invalid check file uid, want 1 issue got %v", got.Issues)
+	}
+	if gotStatus = check.Status(); gotStatus != wantStatus {
+		t.Errorf("invalid check file status, want %v got %v", wantStatus, gotStatus)
+	}
+
+	check.uid = -1
+	check.gid = UNAVAILABLE_GID
+	if got = check.Run(); got.IsOK {
+		t.Error("invalid check file gid, want not ok got ok")
+	} else if len(got.Issues) != 1 || !strings.Contains(fmt.Sprintf("%v", got.Issues[0]), "group mismatch") {
+		t.Errorf("invalid check file gid, want 1 issue got %v", got.Issues)
+	}
+	if gotStatus = check.Status(); gotStatus != wantStatus {
+		t.Errorf("invalid check file status, want %v got %v", wantStatus, gotStatus)
+	}
+
+	check = NewCheckFile("LICENSE")
+	check.fileType = TypeDir
+	if got = check.Run(); got.IsOK {
+		t.Error("invalid check dir file, want not ok got ok")
+	} else if len(got.Issues) != 1 || !strings.Contains(fmt.Sprintf("%v", got.Issues[0]), "is not a dir") {
+		t.Errorf("invalid check dir file, want 1 issue got %v", got.Issues)
+	}
+
+	check = NewCheckFile("LICENSE")
+	check.minSize = 0
+	if got = check.Run(); !got.IsOK {
+		t.Error("invalid check file min size, want ok got not ok")
+	}
+	check.minSize = 1024 * 1024 * 100
+	if got = check.Run(); got.IsOK {
+		t.Error("invalid check file min size, want not ok got ok")
+	} else if len(got.Issues) != 1 || !strings.Contains(fmt.Sprintf("%v", got.Issues[0]), "file too small") {
+		t.Errorf("invalid check file min size, want 1 issue got %v", got.Issues)
+	}
+	if gotStatus = check.Status(); gotStatus != wantStatus {
+		t.Errorf("invalid check file status, want %v got %v", wantStatus, gotStatus)
+	}
+
+	check = NewCheckFile("LICENSE")
+	check.maxSize = 1024 * 1024 * 100
+	if got = check.Run(); !got.IsOK {
+		t.Error("invalid check file max size, want ok got not ok")
+	}
+	check.maxSize = 1
+	if got = check.Run(); got.IsOK {
+		t.Error("invalid check file max size, want not ok got ok")
+	} else if len(got.Issues) != 1 || !strings.Contains(fmt.Sprintf("%v", got.Issues[0]), "file too large") {
+		t.Errorf("invalid check file max size, want 1 issue got %v", got.Issues)
+	}
+}
+
+func TestCheckFileDirectories(t *testing.T) {
+	var check *CheckFile
+	var got Result
+
+	check = NewCheckFile("cmd")
+	check.fileType = TypeDir
+	if got = check.Run(); !got.IsOK {
+		t.Error("invalid check dir dir, want ok got not ok")
+	} else if len(got.Issues) > 0 {
+		t.Errorf("invalid check file, want no issues got %v", got.Issues)
+	}
+
+	check.uid = UNAVAILABLE_UID
+	if got = check.Run(); got.IsOK {
+		t.Error("invalid check dir uid, want not ok got ok")
+	} else if len(got.Issues) != 1 || !strings.Contains(fmt.Sprintf("%v", got.Issues[0]), "owner mismatch") {
+		t.Errorf("invalid check dir uid, want 1 issue got %v", got.Issues)
+	}
+
+	check.uid = -1
+	check.gid = UNAVAILABLE_GID
+	if got = check.Run(); got.IsOK {
+		t.Error("invalid check dir gid, want not ok got ok")
+	} else if len(got.Issues) != 1 || !strings.Contains(fmt.Sprintf("%v", got.Issues[0]), "group mismatch") {
+		t.Errorf("invalid check dir gid, want 1 issue got %v", got.Issues)
+	}
+}
+
+func TestCheckDial(t *testing.T) {
+	var check *CheckDial
+	var got, want string
+	check = NewCheckDial()
+	if got = check.Group(); got != want {
+		t.Errorf("invalid check dial group, want empty got %v", got)
+	}
+	want = "tcp:127.0.0.1:80"
+	if got = check.Name(); got != want {
+		t.Errorf("invalid check dial name, want %v got %v", want, got)
+	}
+	var gotStatus, wantStatus Status
+	wantStatus = StatusUnknown
+	if gotStatus = check.Status(); gotStatus != wantStatus {
+		t.Errorf("invalid check dial status, want %v got %v", wantStatus, gotStatus)
+	}
+}
+
+func TestCheckDialTCPPortAbsent(t *testing.T) {
+	var check *CheckDial
+	var got Result
+	var gotStatus, wantStatus Status
+	wantStatus = StatusDone
+	check = NewCheckDial()
+	check.Address = fmt.Sprintf("localhost:%d", UNAVAILABLE_PORT)
+	check.Timeout, _ = time.ParseDuration("500ms")
+	check.Absent = true
+	if got = check.Run(); !got.IsOK {
+		t.Fatalf("invalid check dial, want ok got not ok")
+	}
+	if gotStatus = check.Status(); gotStatus != wantStatus {
+		t.Errorf("invalid check dial status, want %v got %v", wantStatus, gotStatus)
+	}
+}
+
+func TestCheckDialTCPPort(t *testing.T) {
+	var check *CheckDial
+	var got Result
+	var gotStatus, wantStatus Status
+	wantStatus = StatusDone
+	check = NewCheckDial()
+	check.Timeout, _ = time.ParseDuration("500ms")
+	check.Address = fmt.Sprintf("localhost:%d", UNAVAILABLE_PORT)
+	if got = check.Run(); got.IsOK {
+		t.Fatalf("invalid check dial, want not ok got ok")
+	}
+	if gotStatus = check.Status(); gotStatus != wantStatus {
+		t.Errorf("invalid check dial status, want %v got %v", wantStatus, gotStatus)
+	}
+}
